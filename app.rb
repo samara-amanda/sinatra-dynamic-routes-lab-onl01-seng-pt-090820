838a9a60e3ce7f8d01d22d2989766da3ce891e7c
Samara Grant <samara.amanda01@gmail.com>
2020-11-23 02:03:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    @user_name = params[:name]
+    "<bdo dir="rtl">@user_name</bdo>"
+  end
+    
+  end
 
 end
