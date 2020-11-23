d32ba486833f5cfb3921fe8628c6488c5d5b685b
Samara Grant <samara.amanda01@gmail.com>
2020-11-23 02:19:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    @operation = params[:operation]
+    @oper = params[:operation]
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
     case @oper
