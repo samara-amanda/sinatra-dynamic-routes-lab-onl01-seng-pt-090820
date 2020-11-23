06cc9f1cb0160183c1094332b3b88d94dff23281
Samara Grant <samara.amanda01@gmail.com>
2020-11-23 02:13:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,7 +17,16 @@ class App < Sinatra::Base
     @num = params[:number].to_i
     @phrase = params[:phrase]
     @num.times {str += "#{@phrase}/n" }
-    str    
+    str
   end
 
+  get ''/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+
+  end
 end
